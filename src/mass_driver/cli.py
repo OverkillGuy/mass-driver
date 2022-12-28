fa662b66f1d6edc88f565017284979b9d3a06a7c
Jb DOYON <jb@jiby.tech>
2022-12-27 22:27:01 -0400

    Refactor driver plugin discovery

--- src/mass_driver/cli.py
+++ src/mass_driver/cli.py
@@ -1,40 +1,48 @@
 """Command line entrypoint for mass-driver"""
-import argparse
 import os
 import sys
+from argparse import ArgumentParser, FileType, Namespace
 from pathlib import Path
 
-from mass_driver.discovery import discover_drivers, load_drivers
+from mass_driver.discovery import get_driver
 from mass_driver.main import main
 
 
-def parse_arguments(arguments: list[str]) -> argparse.Namespace:
-    """Parse generic arguments, given as parameters"""
-    parser = argparse.ArgumentParser(
+def gen_parser() -> ArgumentParser:
+    """Create the initial parser"""
+    parser = ArgumentParser(
         "mass-driver",
         description="Send bulk repo change requests",
+    )
+    # parser.add_argument(
+    #     "--token-file",
+    #     help="File containing Github API Token",
+    #     type=argparse.FileType("r"),
+    # )
+    return parser
+
+
+def subparsers(parser: ArgumentParser) -> ArgumentParser:
+    """Add subparsers for driver selection"""
+    subparser = parser.add_subparsers(dest="cmd", title="Commands")
+    subparser.required = True
+    drivers = subparser.add_parser(
+        "drivers",
+        help="Inspect drivers (Plugins)",
+    )
+    drivers.add_argument("--list", action="store_true", help="List available drivers")
+    drivers.add_argument("--info", help="Show docs of a specific driver")
+    drivers.set_defaults(func=drivers_command)
+    run = subparser.add_parser(
+        "run",
+        help="Run mass-driver over multiple repos",
         epilog="Github API token requires either --token-file flag or envvar GITHUB_API_TOKEN\nCurrently no driver selection",
     )
-    repolist_group = parser.add_mutually_exclusive_group(required=True)
-    repolist_group.add_argument(
-        "--repo-path",
-        nargs="*",
-        help="One or more Repositories to patch. If not local paths, will git clone them",
-    )
-    repolist_group.add_argument(
-        "--repo-filelist",
-        type=argparse.FileType("r"),
-        help="File with list of Repositories to Patch. If not local paths, will git clone them",
-    )
-    parser.add_argument(
-        "--branch-name",
-        help="Name of the patch branch. Defaults to the PatchDriver's classname",
-    )
-    parser.add_argument(
+    run.add_argument(
         "driver",
-        help="Name of the driver to use",
+        help="Name of the driver to apply",
     )
-    detect_group = parser.add_mutually_exclusive_group()
+    detect_group = run.add_mutually_exclusive_group()
     detect_group.add_argument(
         "--really-commit-changes",
         dest="dry_run",
@@ -47,28 +55,61 @@ def parse_arguments(arguments: list[str]) -> argparse.Namespace:
         dest="dry_run",
         help="Dry run, no actual commit, no pushing (default)",
     )
-    parser.set_defaults(dry_run=True)
-    parser.add_argument(
-        "--token-file",
-        help="File containing Github API Token",
-        type=argparse.FileType("r"),
+    run.set_defaults(dry_run=True, func=run_command)
+    repolist_group = run.add_mutually_exclusive_group(required=True)
+    repolist_group.add_argument(
+        "--repo-path",
+        nargs="*",
+        help="One or more Repositories to patch. If not local paths, will git clone them",
     )
+    repolist_group.add_argument(
+        "--repo-filelist",
+        type=FileType("r"),
+        help="File with list of Repositories to Patch. If not local paths, will git clone them",
+    )
+    run.add_argument(
+        "--branch-name",
+        help="Name of the patch branch. Defaults to the PatchDriver's classname",
+    )
+    return parser
+
+
+def parse_arguments(arguments: list[str]) -> Namespace:
+    """Parse generic arguments, given as parameters"""
+    parser = subparsers(gen_parser())
     return parser.parse_args(arguments)
+
+
+def drivers_command(args: Namespace):
+    """Process the CLI for 'Drivers' subcommand"""
+    print("Drivers subcommand!")
+    print(args)
+
+
+def run_command(args: Namespace):
+    """Process the CLI for 'run' subcommand"""
+    print("Run mode!")
+    if args.repo_filelist:
+        args.repo_path = args.repo_filelist.read().strip().split("\n")
+    token = get_token(args)
+    driver_class = get_driver(args.driver)
+    driver_instance = driver_class(
+        **{
+            "op": "add",
+            "path": "/foo",
+            "value": "bar",
+            "target_file": Path("test.json"),
+        }
+    )
+    main(driver_instance, args.repo_path, args.dry_run, args.branch_name, token)
 
 
 def cli(arguments: list[str] | None = None):
     """Run the mass_driver cli"""
     if arguments is None:
         arguments = sys.argv[1:]
-    args = parse_arguments(arguments)
-    if args.repo_filelist:
-        args.repo_path = args.repo_filelist.read().strip().split("\n")
-    token = get_token(args)
-    driver_class = get_driver(args.driver)
-    driver_instance = driver_class(
-        target_file=Path("test.json"), **{"op": "add", "path": "/foo", "value": "bar"}
-    )
-    main(driver_instance, args.repo_path, args.dry_run, args.branch_name, token)
+    pargs = parse_arguments(arguments)
+    pargs.func(pargs)  # Dispatch to the subcommand func (drivers/run)
 
 
 def get_token(args) -> str:
@@ -84,12 +125,3 @@ def get_token(args) -> str:
         )
         exit(2)  # Simulate the argparse behaviour of exiting on bad args
     return token
-
-
-def get_driver(driver_name: str):
-    """Fetch the given driver, by name"""
-    discovered = discover_drivers()
-    drivers = load_drivers(discovered)
-    if driver_name not in drivers:
-        raise ImportError("Driver not found")
-    return drivers[driver_name]
--- src/mass_driver/discovery.py
+++ src/mass_driver/discovery.py
@@ -5,18 +5,22 @@ import sys
 from mass_driver.model import PatchDriver
 
 if sys.version_info < (3, 10):
-    from importlib_metadata import EntryPoint, entry_points
+    from importlib_metadata import EntryPoints, entry_points
 else:
-    from importlib.metadata import EntryPoint, entry_points
+    from importlib.metadata import EntryPoints, entry_points
 
 DriverName = str
 
 
-def discover_drivers() -> list[EntryPoint]:
+def discover_drivers() -> EntryPoints:
     """Discover all Drivers via plugin system"""
     return entry_points(group="massdriver.drivers")
 
 
-def load_drivers(discovered_drivers: list[EntryPoint]) -> dict[DriverName, PatchDriver]:
-    """Load all discovered drivers as map of name->callable"""
-    return {driver.name: driver.load() for driver in discovered_drivers}
+def get_driver(driver_name: str) -> type[PatchDriver]:
+    """Fetch the given driver class, by name"""
+    drivers = discover_drivers()
+    if driver_name not in drivers.names:
+        raise ImportError(f"Driver '{driver_name}' not found in 'massdriver.drivers'")
+    (driver,) = drivers.select(name=driver_name)
+    return driver.load()
