c421e42d7627fc37ba5fe44158437e1f30ceccf1
Jb DOYON <jb@jiby.tech>
2023-06-03 23:39:51 +0100

    Refactor plugin discovery
    Common plugin discovery functions

--- src/mass_driver/discovery.py
+++ src/mass_driver/discovery.py
@@ -1,6 +1,7 @@
 """Driver discovery system via plugins"""
 
 from importlib.metadata import EntryPoint, EntryPoints, entry_points
+from typing import Callable
 
 from mass_driver.models.forge import Forge
 from mass_driver.models.patchdriver import PatchDriver
@@ -21,39 +22,42 @@ def discover_drivers() -> EntryPoints:
     return entry_points(group=DRIVER_ENTRYPOINT)
 
 
-def get_driver_entrypoint(driver_name: str) -> EntryPoint:
-    """Fetch the given driver Entrypoint, by name"""
-    drivers = discover_drivers()
-    if driver_name not in drivers.names:
-        raise ImportError(f"Driver '{driver_name}' not found in '{DRIVER_ENTRYPOINT}'")
-    (driver,) = drivers.select(name=driver_name)
-    return driver
-
-
-def get_driver(driver_name: str) -> type[PatchDriver]:
-    """Get the given driver Class, by entrypoint name"""
-    driver = get_driver_entrypoint(driver_name)
-    return driver.load()
-
-
 def discover_forges() -> EntryPoints:
     """Discover all Forges via plugin system"""
     return entry_points(group=FORGE_ENTRYPOINT)
 
 
+def get_plugin_entrypoint(
+    plugin: str, name: str, entrypoint: str, discover: Callable
+) -> EntryPoint:
+    """Fetch the given plugin's Entrypoint, by name"""
+    plugin_objs = discover()
+    if name not in plugin_objs.names:
+        raise ImportError(f"{plugin} '{name}' not found in '{entrypoint}'")
+    (plugin_obj,) = plugin_objs.select(name=name)
+    return plugin_obj
+
+
+def get_driver_entrypoint(driver_name: str) -> EntryPoint:
+    """Fetch the given driver Entrypoint, by name"""
+    return get_plugin_entrypoint(
+        "driver", driver_name, DRIVER_ENTRYPOINT, discover_drivers
+    )
+
+
 def get_forge_entrypoint(forge_name: str) -> EntryPoint:
     """Fetch the given forge Entrypoint, by name"""
-    forges = discover_forges()
-    if forge_name not in forges.names:
-        raise ImportError(f"Forge '{forge_name}' not found in '{FORGE_ENTRYPOINT}'")
-    (forge,) = forges.select(name=forge_name)
-    return forge
+    return get_plugin_entrypoint("forge", forge_name, FORGE_ENTRYPOINT, discover_forges)
+
+
+def get_driver(driver_name: str) -> type[PatchDriver]:
+    """Get the given driver Class, by entrypoint name"""
+    return get_driver_entrypoint(driver_name).load()
 
 
 def get_forge(forge_name: str) -> type[Forge]:
     """Get the given forge Class, by entrypoint name"""
-    forge = get_forge_entrypoint(forge_name)
-    return forge.load()
+    return get_forge_entrypoint(forge_name).load()
 
 
 def get_scanners() -> list[Scanner]:
