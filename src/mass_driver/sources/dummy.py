040afbcd1c38d6782ec80359704085e22a3975e1
Jb DOYON <jb@jiby.tech>
2023-06-04 23:37:06 +0100

    Add templated repo-filelist source
    Enables a simple repo template to be used for a list of repos

--- src/mass_driver/sources/dummy.py
+++ src/mass_driver/sources/dummy.py
@@ -29,3 +29,23 @@ class RepoFilelistSource(Source):
             raise ValueError(f"Repo-file path not a real file:  '{self.repo_file}'")
         repo_list = repo_file_path.read_text().strip().split("\n")
         return [Repo(clone_url=url, repo_id=url) for url in repo_list]
+
+
+class TemplateFileSource(Source):
+    """A Source that reads repo id from file, templating the clone URL around it"""
+
+    repo_file: str
+    """The path to the newline-delimited file that holds repo ids"""
+    clone_url_template: str
+    """The repo clone URL template string, ready to inject ID into. Must contain {id}"""
+
+    def discover(self) -> list[Repo]:
+        """Discover a list of repositories"""
+        repo_file_path = Path(self.repo_file)
+        if not repo_file_path.is_file():
+            raise ValueError(f"Path not a real file:  '{self.repo_file}'")
+        id_list = repo_file_path.read_text().strip().split("\n")
+        return [
+            Repo(clone_url=self.clone_url_template.format(id=repo_id), repo_id=repo_id)
+            for repo_id in id_list
+        ]
