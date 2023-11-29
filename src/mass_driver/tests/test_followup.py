29a59ddce0dd1e67cd5a6f62c56ed60e4a128f71
Jb DOYON <jb@jiby.tech>
2023-11-29 23:55:07 +0000

    Add xfail test for clone fail forwarding to patch

--- src/mass_driver/tests/test_followup.py
+++ src/mass_driver/tests/test_followup.py
@@ -6,6 +6,7 @@ import pytest
 from mass_driver.forge_run import PROutcome
 from mass_driver.forges.dummy import DUMMY_PR_URL
 from mass_driver.models.patchdriver import PatchOutcome
+from mass_driver.models.status import Phase
 from mass_driver.tests.fixtures import (
     copy_folder,
     massdrive_runlocal,
@@ -16,6 +17,7 @@ from mass_driver.tests.fixtures import (
 @pytest.mark.xfail(reason="2-mig,1forge known bug")
 def test_2migration_but_1forge(tmp_path, shared_datadir, monkeypatch):
     """Scenario: Forge a single time when 2 migration but only one fails"""
+    # Given two repos to migrate
     repo1_path = Path(tmp_path / "test_repo1/")
     copy_folder(Path(shared_datadir / "sample_repo"), repo1_path)
     repoize(repo1_path)
@@ -47,6 +49,44 @@ def test_2migration_but_1forge(tmp_path, shared_datadir, monkeypatch):
     forge1 = result.forge_result[repo1_id]
     if forge1 is None:
         return pytest.fail("Should have a forge result for mig1")
+
+    assert forge1.outcome == PROutcome.PR_CREATED, "Should succeed creating PR"
+    # And I get the PR URL I want
+    assert forge1.pr_html_url == DUMMY_PR_URL, "Should have returned correct PR URL"
+
+
+@pytest.mark.xfail(reason="clonefail, sent for PR known bug")
+def test_1badclone_but_1forge(tmp_path, shared_datadir, monkeypatch):
+    """Scenario: Forge a single time given 2 migration but 1 failed cloning"""
+    # Given two repos to migrate
+    repo1_path = Path(tmp_path / "test_repo1/")
+    copy_folder(Path(shared_datadir / "sample_repo"), repo1_path)
+    repoize(repo1_path)
+    repo1_id = "./test_repo1"
+
+    # But the second one doesn't exist on disk
+    repo2_id = "./test_repo2"
+
+    activityconfig_filepath = shared_datadir / "2mig_1forge.toml"
+    # When I run mass-driver with 2 migrations to forge
+    result = massdrive_runlocal(None, activityconfig_filepath)
+    assert result is not None, "Should have a result"
+    assert result.migration_result is not None, "Should have a migration result"
+    mig1 = result.migration_result[repo1_id]
+    mig2 = result.migration_result[repo2_id]
+    # Then I get OK outcome on migration1
+    assert mig1.outcome == PatchOutcome.PATCHED_OK, "Wrong outcome from patching"
+    # And I get Clone error outcome on migration2
+    assert mig2.outcome == PatchOutcome.PATCH_ERROR, "Wrong outcome from patching"
+    error = mig2.error
+    assert error is not None, "Should have gotten a record of the error"
+    assert error.activity == Phase.CLONE, "Error should be marked from Clone activity"
+
+    # And I get only 1 forge result
+    assert result.forge_result is not None, "Should have a forge result"
+    assert len(result.forge_result.keys()) == 1, "Should get only one forge result"
+    forge1 = result.forge_result[repo1_id]
+    assert forge1 is not None, "Should have a forge result for mig1"
 
     assert forge1.outcome == PROutcome.PR_CREATED, "Should succeed creating PR"
     # And I get the PR URL I want
